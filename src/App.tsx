cdda8c16a2c6558a26407797a8878ae3b1938437
Borislav Gotsev <anatoliyspasov9@gmail.com>
2025-08-20 08:37:30 +0300

    Fix missing src/main.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Li Prestige Stroy</h1>
+      <p>Premium Construction Services</p>
+    </div>
+  )
+}
+
+export default App
